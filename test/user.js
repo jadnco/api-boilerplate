06e3d442a01f5f3fd3c501d020aea06c2a7172a0
Jaden Dessureault <jaden.dessureault@gmail.com>
2016-02-14 14:20:40 -0600

    Add example test

--- test/user.js
+++ test/user.js
@@ -1,1 +1,27 @@
-// Tests for user api requests
+'use strict';
+
+import {expect} from 'chai';
+import Request from 'request-promise';
+import IP from 'ip';
+
+const route = `http://${IP.address()}:1998/api/users`;
+
+describe('User Model', () => {
+  describe('GET single user', () => {
+    let options = {
+      uri: `${route}/56b95ffa9a663798f7c9833`,
+      method: 'GET',
+      resolveWithFullResponse: true,
+    };
+
+    it('should have content-type of json', done => {
+      Request(options)
+        .then(res => {
+          expect(res.headers['content-type']).to.contain('application/json');
+
+          done();
+        })
+        .catch(done);
+    });
+  });
+});
